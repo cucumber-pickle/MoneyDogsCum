e9b4e7f09eff256bf113aa3c3a12841d2cad2462
cucumber <coma2ss@gmail.com>
2024-11-14 00:23:03 +0300

    update tasks

--- bot.py
+++ bot.py
@@ -2,9 +2,12 @@ import requests
 import json
 import os
 from colorama import *
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 from core.helper import get_headers, countdown_timer, extract_user_data, config
 import random
+import time
+from platform import system as s_name
+from os import system as sys
 
 class MoneyDOGS:
     def __init__(self):
@@ -35,6 +38,17 @@ class MoneyDOGS:
         print(Fore.YELLOW + f" before start please '{Fore.GREEN}git pull{Fore.YELLOW}' to update bot")
         print(f"{Fore.WHITE}~" * 60)
 
+    def set_proxy(self, proxy):
+        self.session.proxies = {
+            "http": proxy,
+            "https": proxy,
+        }
+        if '@' in proxy:
+            host_port = proxy.split('@')[-1]
+        else:
+            host_port = proxy.split('//')[-1]
+        return host_port
+
     def format_seconds(self, seconds):
         hours, remainder = divmod(seconds, 3600)
         minutes, seconds = divmod(remainder, 60)
@@ -49,39 +63,21 @@ class MoneyDOGS:
 
     def get_token(self, query: str):
         url = 'https://api.moneydogs-ton.com/sessions'
-        data = json.dumps({'encodedMessage': query})
+        data = json.dumps({'encodedMessage': query, 'retentionCode': '48cdRxLi'})
         self.headers.update({
             'Content-Length': str(len(data)),
             'Content-Type': 'application/json'
         })
 
-        try:
-            response = self.session.post(url, headers=self.headers, data=data, timeout=10)  # Set timeout
-            data = response.json()
-            if response.status_code == 200:
-                return data['token']
-            else:
-                return None
-        except requests.exceptions.Timeout:
-            self.log("Request timed out. Please check your network connection or proxy settings.")
-            return None
-        except requests.exceptions.RequestException as e:
-            self.log(f"An error occurred: {e}")
-            return None
-
-    def set_proxy(self, proxy):
-        self.session.proxies = {
-            "http": proxy,
-            "https": proxy,
-        }
-        if '@' in proxy:
-            host_port = proxy.split('@')[-1]
+        response = self.session.post(url, headers=self.headers, data=data)
+        data = response.json()
+        if response.status_code == 200:
+            return data['token']
         else:
-            host_port = proxy.split('//')[-1]
-        return host_port
+            return None
 
     def user_info(self, token: str):
-        url = 'https://api.moneydogs-ton.com/rankings/deposits/me'
+        url = 'https://api.moneydogs-ton.com/mdogs-deposits'
         self.headers.update({
             'Content-Length': '0',
             'Content-Type': 'application/json',
@@ -94,7 +90,7 @@ class MoneyDOGS:
             return data
         else:
             return None
-        
+
     def daily_checkin(self, token: str):
         url = 'https://api.moneydogs-ton.com/daily-check-in'
         self.headers.update({
@@ -109,21 +105,23 @@ class MoneyDOGS:
             return data
         else:
             return None
-        
-    def get_tasks(self, token: str):
-        url = 'https://api.moneydogs-ton.com/tasks'
+
+    def get_tasks(self, token: str, task_type: str = 'all'):
+        base_url = 'https://api.moneydogs-ton.com/tasks'
+        url = f"{base_url}?isFeatured=true" if task_type == 'featured' else base_url
         self.headers.update({
             'Content-Length': '0',
             'Content-Type': 'application/json',
             'X-Auth-Token': token
         })
+
         response = self.session.get(url, headers=self.headers)
         data = response.json()
         if response.status_code == 200:
             return data
         else:
             return None
-        
+
     def complete_tasks(self, token: str, task_id: str):
         url = f'https://api.moneydogs-ton.com/tasks/{task_id}/verify'
         self.headers.update({
@@ -132,123 +130,99 @@ class MoneyDOGS:
         })
 
         response = self.session.post(url, headers=self.headers)
-        if response.status_code == 201:
+        if response.status_code in [200, 201]:
             return True
-        elif response.status_code == 200:
-            try:
-                data = response.json()
-                return data
-            except ValueError:
-                return True
         else:
-            return False
-
-    def save(self, id, token):
-        tokens = json.loads(open("tokens.json").read())
-        tokens[str(id)] = token
-        open("tokens.json", "w").write(json.dumps(tokens, indent=4))
+            return None
 
     def process_query(self, query: str):
-
-        #проверяем есть ли токен в токенс, если нет, то логинимся и сохряняем новый
         token = self.get_token(query)
+        if not token:
+            self.log(
+                f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                f"{Fore.RED + Style.BRIGHT} Query ID Isn't Valid {Style.RESET_ALL}"
+                f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+            )
+            return
 
         if token:
-            user_info = self.user_info(token)
-            if user_info:
-                first_name = user_info['firstName']
-                balance = f"{user_info['score']:.4f}"
+            user = self.user_info(token)
+            if user:
                 self.log(
-                    f"{Fore.CYAN + Style.BRIGHT}[ Account{Style.RESET_ALL}"
-                    f"{Fore.WHITE + Style.BRIGHT} {first_name} {Style.RESET_ALL}"
-                    f"{Fore.CYAN + Style.BRIGHT}] {Style.RESET_ALL}"
-                    f"{Fore.MAGENTA + Style.BRIGHT}[ Balance{Style.RESET_ALL}"
-                    f"{Fore.WHITE + Style.BRIGHT} {balance} MDOGS {Style.RESET_ALL}"
+                    f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                    f"{Fore.WHITE + Style.BRIGHT} {user['user']['firstName']} {Style.RESET_ALL}"
+                    f"{Fore.MAGENTA + Style.BRIGHT}] [ Balance{Style.RESET_ALL}"
+                    f"{Fore.WHITE + Style.BRIGHT} {user['remainingAmount']:.4f} $MDOGS {Style.RESET_ALL}"
                     f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
                 )
-            else:
-                self.log(f"[ User Not Found ]")
+                time.sleep(1)
 
-            print(
-                f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().strftime('%x %X %Z')} ]{Style.RESET_ALL}"
-                f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
-                f"{Fore.YELLOW + Style.BRIGHT}[ Get Daily Check-in... ]{Style.RESET_ALL}",
-                end="\r",
-                flush=True
-            )
-            countdown_timer(2)
-            checkin = self.daily_checkin(token)
-            if checkin:
-                reward = checkin['rewardMdogs']
-                self.log(
-                    f"{Fore.MAGENTA + Style.BRIGHT}[ Check-in{Style.RESET_ALL}"
-                    f"{Fore.GREEN + Style.BRIGHT} Success {Style.RESET_ALL}"
-                    f"{Fore.MAGENTA + Style.BRIGHT}] [ Reward{Style.RESET_ALL}"
-                    f"{Fore.WHITE + Style.BRIGHT} {reward} MDOGS {Style.RESET_ALL}"
-                    f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
-                )
-            else:
-                self.log(
-                    f"{Fore.MAGENTA + Style.BRIGHT}[ Check-in{Style.RESET_ALL}"
-                    f"{Fore.YELLOW + Style.BRIGHT} Already Check-in Today {Style.RESET_ALL}"
-                    f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}      "
-                )
+                checkin = self.daily_checkin(token)
+                if checkin:
+                    self.log(
+                        f"{Fore.MAGENTA + Style.BRIGHT}[ Check-in{Style.RESET_ALL}"
+                        f"{Fore.GREEN + Style.BRIGHT} Is Claimed {Style.RESET_ALL}"
+                        f"{Fore.MAGENTA + Style.BRIGHT}] [ Reward{Style.RESET_ALL}"
+                        f"{Fore.WHITE + Style.BRIGHT} {checkin['rewardMdogs']} $MDOGS {Style.RESET_ALL}"
+                        f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                    )
+                else:
+                    now = datetime.now(timezone.utc)
+                    checkin_time = (now + timedelta(days=1)).replace(hour=0, minute=0, second=0,
+                                                                     microsecond=0).strftime('%x %X %Z')
+                    self.log(
+                        f"{Fore.MAGENTA + Style.BRIGHT}[ Check-in{Style.RESET_ALL}"
+                        f"{Fore.YELLOW + Style.BRIGHT} Not Time to Claim {Style.RESET_ALL}"
+                        f"{Fore.MAGENTA + Style.BRIGHT}] [ Next Claim at{Style.RESET_ALL}"
+                        f"{Fore.WHITE + Style.BRIGHT} {checkin_time} {Style.RESET_ALL}"
+                        f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                    )
+                time.sleep(1)
 
-            print(
-                f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().strftime('%x %X %Z')} ]{Style.RESET_ALL}"
-                f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
-                f"{Fore.YELLOW + Style.BRIGHT}[ Get Available Tasks... ]{Style.RESET_ALL}",
-                end="\r",
-                flush=True
-            )
-            countdown_timer(2)
-            tasks = self.get_tasks(token)
-            manual_task = False
-            if tasks:
-                for task in tasks:
-                    task_id = task['id']
-                    title = task['title']
+                for type in ['featured', 'all']:
+                    tasks = self.get_tasks(token, type)
+                    if tasks:
+                        for task in tasks:
+                            task_id = str(task['id'])
 
-                    if task['code'] is None:
-                        print(
-                            f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().strftime('%x %X %Z')} ]{Style.RESET_ALL}"
-                            f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
-                            f"{Fore.MAGENTA + Style.BRIGHT}[ Task{Style.RESET_ALL}"
-                            f"{Fore.WHITE + Style.BRIGHT} {title} {Style.RESET_ALL}"
-                            f"{Fore.YELLOW + Style.BRIGHT}is Strarting...{Style.RESET_ALL}"
-                            f"{Fore.MAGENTA + Style.BRIGHT} ]{Style.RESET_ALL}",
-                            end="\r",
-                            flush=True
-                        )
-                        countdown_timer(2)
-                        complete_task = self.complete_tasks(token, task_id)
-
-                        if complete_task:
-                            reward = int(task['rewardMdogs'])
+                            if task:
+                                verify = self.complete_tasks(token, task_id)
+                                if verify:
+                                    self.log(
+                                        f"{Fore.MAGENTA + Style.BRIGHT}[ Tasks{Style.RESET_ALL}"
+                                        f"{Fore.WHITE + Style.BRIGHT} {task['title']} {Style.RESET_ALL}"
+                                        f"{Fore.GREEN + Style.BRIGHT}Is Completed{Style.RESET_ALL}"
+                                        f"{Fore.MAGENTA + Style.BRIGHT} ] [ Reward{Style.RESET_ALL}"
+                                        f"{Fore.WHITE + Style.BRIGHT} {task['rewardMdogs']:.1f} $MDOGS {Style.RESET_ALL}"
+                                        f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                                    )
+                                else:
+                                    self.log(
+                                        f"{Fore.MAGENTA + Style.BRIGHT}[ Tasks{Style.RESET_ALL}"
+                                        f"{Fore.WHITE + Style.BRIGHT} {task['title']} {Style.RESET_ALL}"
+                                        f"{Fore.RED + Style.BRIGHT}Isn't Completed{Style.RESET_ALL}"
+                                        f"{Fore.MAGENTA + Style.BRIGHT} ]{Style.RESET_ALL}"
+                                    )
+                                time.sleep(1)
+                    else:
+                        if tasks == 'featured':
                             self.log(
-                                f"{Fore.MAGENTA + Style.BRIGHT}[ Task{Style.RESET_ALL}"
-                                f"{Fore.WHITE + Style.BRIGHT} {title} {Style.RESET_ALL}"
-                                f"{Fore.GREEN + Style.BRIGHT}is Completed{Style.RESET_ALL}"
-                                f"{Fore.MAGENTA + Style.BRIGHT} ] [ Reward{Style.RESET_ALL}"
-                                f"{Fore.WHITE + Style.BRIGHT} {reward} MDOGS {Style.RESET_ALL}"
-                                f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}          "
+                                f"{Fore.MAGENTA + Style.BRIGHT}[ Partner Tasks{Style.RESET_ALL}"
+                                f"{Fore.GREEN + Style.BRIGHT} Is Completed {Style.RESET_ALL}"
+                                f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
                             )
                         else:
                             self.log(
-                                f"{Fore.MAGENTA + Style.BRIGHT}[ Task{Style.RESET_ALL}"
-                                f"{Fore.WHITE + Style.BRIGHT} {title} {Style.RESET_ALL}"
-                                f"{Fore.RED + Style.BRIGHT}is Failed{Style.RESET_ALL}"
-                                f"{Fore.MAGENTA + Style.BRIGHT} ]{Style.RESET_ALL}                          "
+                                f"{Fore.MAGENTA + Style.BRIGHT}[ General Tasks{Style.RESET_ALL}"
+                                f"{Fore.GREEN + Style.BRIGHT} Is Completed {Style.RESET_ALL}"
+                                f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
                             )
-                    else:
-                        manual_task = True
-
-                if manual_task:
-                    self.log(f"{Fore.YELLOW + Style.BRIGHT}[Other tasks require authentication]{Style.RESET_ALL}")
-        else:
-            self.log(f"{Fore.RED+Style.BRIGHT}[Check Proxy!]{Style.RESET_ALL}")
-
-
+            else:
+                self.log(
+                    f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                    f"{Fore.RED + Style.BRIGHT} Data Is None {Style.RESET_ALL}"
+                    f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                )
     def main(self):
         try:
             with open('query.txt', 'r') as file:
@@ -293,15 +267,17 @@ class MoneyDOGS:
                         user_id = str(user_info.get('id'))
 
                         self.headers = get_headers(user_id)
-
-                        self.process_query(query)
+                        try:
+                            self.process_query(query)
+                        except Exception as e:
+                            self.log(f"{Fore.RED + Style.BRIGHT}An error process_query: {e}{Style.RESET_ALL}")
 
                         self.log(
                             f"{Fore.CYAN + Style.BRIGHT}----------------------------------------------------------------------------{Style.RESET_ALL}")
 
                         account_delay = config['account_delay']
                         countdown_timer(random.randint(min(account_delay), max(account_delay)))
-                
+
                 cycle_delay = config['cycle_delay']
                 countdown_timer(random.randint(min(cycle_delay), max(cycle_delay)))
 
@@ -311,6 +287,10 @@ class MoneyDOGS:
             self.log(f"{Fore.RED + Style.BRIGHT}An error occurred: {e}{Style.RESET_ALL}")
 
 if __name__ == "__main__":
+    if s_name() == 'Windows':
+        sys(f'cls && title Money Dogs')
+    else:
+        sys('clear')
     moneydogs = MoneyDOGS()
     moneydogs.clear_terminal()
     moneydogs.welcome()
